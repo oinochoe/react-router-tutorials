fe9590c69bb733f3c7722d767dba781d5371d9f5
oinochoe <oinochoe@naver.com>
2021-02-14 13:54:19 +0900

    query string 가져오기 파싱

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Profile from './Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li><Link to="/">홈</Link></li>
+        <li><Link to="/about">소개</Link></li>
+      </ul>
+      <Route path="/" component={Home} exact />
+      <Route path="/about" component={About} />
+      <Route path="/profiles/:username" component={Profile} />
     </div>
   );
 }
